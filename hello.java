47f48f7f6689bde2facc9c1c812403fae8de55a1
vrinsakun <venikrinsakun@gmail.com>
2019-01-30 17:49:03 -0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,11 @@
-//testing ability 
+//Name :
+//Assignment :
+//Description : testing VS Code and Git
 
 public class hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!");
     }
 }
